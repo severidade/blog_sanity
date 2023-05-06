3675078d8036ff911871edca417e9abadd43eceb
severo <oliveira.severo@gmail.com>
2023-05-06 03:06:04 -0300

    refactor: :recycle: Organiza o componente nav
    Ele agora usa css module

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styles from './Navbar.module.css';
 
 export default function Navbar() {
   return(
-    <header>
+    <header className={ styles.container_header }>
       <NavLink to="/"><h1>BLOG</h1></NavLink>
-      <nav>
+      <nav className={ styles.container_nav }>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/post">Blog Posts</NavLink>
         <NavLink to="/projects">Projetos</NavLink>
