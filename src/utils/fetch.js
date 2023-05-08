bc6054ee6d933f1e9932a3a9a7b67e8edd9c0f9a
severo <oliveira.severo@gmail.com>
2023-05-08 10:05:59 -0300

    refactor: :recycle: Refatora a função fetch

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,65 +1,74 @@
 import sanityClient from "../cliente";
 
+const POST_TYPE = "post";
+const PROJECT_TYPE = "project";
+
+const MAIN_IMAGE_FIELDS = `
+  mainImage{
+    asset->{
+      _id,
+      url,
+    },
+    alt
+  }
+`;
+
+const THUMBNAIL_IMAGE_FIELDS = `
+  thumbnailImage {
+    asset -> {
+      _id,
+      url
+    },
+    alt
+  }
+`;
+
+const POST_FIELDS = `
+  title,
+  sub_title,
+  slug,
+  publishedAt,
+  ${MAIN_IMAGE_FIELDS},
+  ${THUMBNAIL_IMAGE_FIELDS}
+`;
+
+const SINGLE_POST_FIELDS = `
+  title,
+  sub_title,
+  _id,
+  slug,
+  body,
+  ${MAIN_IMAGE_FIELDS},
+  "name": author -> name,
+  "authorImage": author -> image
+`;
+
+const PROJECT_FIELDS = `
+  title,
+  data,
+  place,
+  description,
+  ${MAIN_IMAGE_FIELDS},
+  projectType,
+  link,
+  tags
+`;
+
 export async function fetchPosts() {
-  const data = await sanityClient.fetch(`*[_type == "post"] | order(publishedAt desc) {
-    title,
-    sub_title,
-    slug,
-    mainImage{
-      asset-> {
-        _id,
-        url
-      },
-      alt
-    },
-    thumbnailImage{
-      asset-> {
-        _id,
-        url
-      },
-      alt
-    },
-    publishedAt
-  }`);
+  const query = `*[_type == "${POST_TYPE}"] | order(publishedAt desc) {${POST_FIELDS}}`;
+  const data = await sanityClient.fetch(query);
   return data;
 }
 
 export async function fetchSinglePost(slug) {
-  const data = await sanityClient.fetch(`*[slug.current == "${slug}"]{
-    title,
-    sub_title,
-    _id,
-    slug,
-    mainImage{
-      asset->{
-        _id,
-        url,
-      }
-    },
-    body,
-    "name": author->name,
-    "authorImage": author->image
-  }`);
+  const query = `*[slug.current == "${slug}"] {${SINGLE_POST_FIELDS}}`;
+  const data = await sanityClient.fetch(query);
   return data;
 }
 
 export async function fetchProjects() {
-  const data = await sanityClient.fetch(
-  `*[_type == "project"] | order(publishedAt desc) {
-    title,
-    data,
-    place,
-    description,
-    mainImage{
-      asset->{
-        _id,
-        url,
-      }
-    },
-    projectType,
-    link,
-    tags
-  }`);
+  const query = `*[_type == "${PROJECT_TYPE}"] | order(publishedAt desc) {${PROJECT_FIELDS}}`;
+  const data = await sanityClient.fetch(query);
   return data;
 }
 
