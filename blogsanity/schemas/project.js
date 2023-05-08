87af21ccd1f616e84f32cc1f3caa2733625bd387
severo <oliveira.severo@gmail.com>
2023-05-08 08:44:45 -0300

    feat: :rocket: cria o campo de imagem em projetos

--- blogsanity/schemas/project.js
+++ blogsanity/schemas/project.js
@@ -1,25 +1,35 @@
-export default {
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
   name: "project",
   title: "Project",
   type: "document",
   fields: [
-    {
+    defineField({
       name: "title",
       type: "string"
-    },
-    {
+    }),
+    defineField({
       name: "date",
       type: "datetime",
-    },
-    {
+    }),
+    defineField({
       name: "place",
       type: "string",
-    },
-    { 
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({ 
       name: "description",
       type: "text",
-    },
-    {
+    }),
+    defineField({
       name: "projectType",
       title: "Project Type",
       type: "string",
@@ -30,12 +40,12 @@ export default {
           { value: "school", title: "School"},
         ]
       }
-    },
-    {
+    }),
+    defineField({
       name: "link",
       type: "url",
-    },
-    {
+    }),
+    defineField({
       name: "tags",
       type: "array",
       of: [
@@ -46,6 +56,6 @@ export default {
       options: {
         layout: "tags",
       },
-    },
+    }),
   ],
-};
+});
