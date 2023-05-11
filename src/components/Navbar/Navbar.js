283deabe891ff4cc7f9fb759c2180840b8a92eec
severo <oliveira.severo@gmail.com>
2023-05-11 16:37:24 -0300

    feat: :rocket: Implementa o menu para versão mobile do blog

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 export default function Navbar() {
+  const [menuOpen, setmenuOpen] = useState(false);
+
+  useEffect(() => {
+    document.body.style.overflow = menuOpen ? 'hidden' : 'unset';
+  }, [menuOpen]);
+
+  const toggleMenu = () => {
+    setmenuOpen(!menuOpen);
+  };
 
   return(
     <header className="container_header">
-      <NavLink to="/"><h1>BLOG</h1></NavLink>
-      <nav className="container_nav">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/post">Blog Posts</NavLink>
-        <NavLink to="/projects">Projetos</NavLink>
+      <NavLink to="/">
+        <h1>BLOG</h1>
+      </NavLink>
+      <div
+        className='hamburger'
+        onClick={toggleMenu}>
+        menu
+      </div>
+      <nav className={`container_nav ${menuOpen ? 'open' : ''}`}>
+        <div
+          className='close_menu'
+          onClick={toggleMenu}>close
+        </div>
+        <NavLink to="/" onClick={toggleMenu}>Home</NavLink>
+        <NavLink to="/post" onClick={toggleMenu}>Blog Posts</NavLink>
+        <NavLink to="/projects"onClick={toggleMenu} >Projetos</NavLink>
       </nav>
     </header>
   )
