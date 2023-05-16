e04e2b7daf1f58b01adfd5466774869199a0a45a
severo <oliveira.severo@gmail.com>
2023-05-16 11:10:34 -0300

    feat: :rocket: Implementa o botao de play no vídeo

--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -5,6 +5,7 @@ import BlockContent from '@sanity/block-content-to-react';
 
 export default function Videos() {
   const [videos, setVideos] = useState([]);
+  const [playVideoId, setPlayVideoId] = useState(null);
 
   useEffect(() => {
     const fetchVideos = async () => {
@@ -19,6 +20,10 @@ export default function Videos() {
     fetchVideos();
   }, []);
 
+  const handlePlayVideo = (videoId) => {
+    setPlayVideoId(videoId);
+  };
+
   return (
     <main className="container_main">
       <section className="container_section">
@@ -27,15 +32,33 @@ export default function Videos() {
           <div key={video._id} className='video'>
             <p dangerouslySetInnerHTML={{__html: formatDate(video.publishedAt)}}></p>
             <p>{video.title}</p>
-            <iframe
-              width="100%"
-              height="315"
-              src={`https://www.youtube.com/embed/${video.video?.youtubeId}`}
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
+            <div className="video-container">
+              {playVideoId === video._id ? (
+                <iframe
+                  width="100%"
+                  height="56.25%"
+                  // Proporção de aspecto 16:9 (9 / 16 * 100%)
+                  src={`https://www.youtube.com/embed/${video.video?.youtubeId}?autoplay=1`}
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              ) : (
+                <div>
+                  <img
+                    src={`https://img.youtube.com/vi/${video.video?.youtubeId}/maxresdefault.jpg`}
+                    alt={`Thumbnail do vídeo ${video.title}`}
+                  />
+                  <button
+                    className="play-button"
+                    onClick={() => handlePlayVideo(video._id)}
+                  >
+                    Play
+                  </button>
+                </div>
+              )}
+            </div>
             <BlockContent
               blocks={video.body}
               projectId="70kqnxpw"
@@ -43,7 +66,6 @@ export default function Videos() {
             />
           </div>
         ))}
-
       </section>
     </main>
   );
