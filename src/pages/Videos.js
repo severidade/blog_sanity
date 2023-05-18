f2dcff696ecc2cb06e50bbe91bc73c985037a4ae
severo <oliveira.severo@gmail.com>
2023-05-18 15:56:48 -0300

    refactor: :recycle: Limpa o estado global  ao sair da página vídeos
    Impede que o úlimo vídeo viste execute novamente ao entrar na página que lista todos os vídeos

--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -2,14 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { fetchVideos } from '../utils/fetch';
 import SingleVideo from '../components/SingleVideo/SingleVideo';
 
+import { useDispatch } from 'react-redux';
+import { setPlayingVideoId } from '../store';
+
 export default function Videos() {
   const [videos, setVideos] = useState(null);
+  const dispatch = useDispatch();
+  // useDispatch é um hook fornecido pela biblioteca react-redux que permite que componentes do React disparem ações para a store do Redux.
 
   useEffect(() => {
     fetchVideos()
       .then((data) => setVideos(data))
       .catch(console.error);
-  }, []);
+    
+      // Limpa o estado global ao sair da página
+    return () => {
+      dispatch(setPlayingVideoId(null)); 
+    };
+  }, [dispatch]);
+
+  
 
   if (!videos) return <div className='loading'>Loading...</div>;
 
