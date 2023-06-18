fab98e6dc3b400bb1c09f69f8b5e28c482cbdf37
severo <oliveira.severo@gmail.com>
2023-06-18 11:14:07 -0300

    feat: :rocket: Rola a página para o topo quando seleciona um item da paginacao

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -3,6 +3,14 @@ import ReactPaginate from 'react-paginate';
 import './Pagination.css';
 
 export default function Pagination({ pageCount, handlePageClick, currentPage }) {
+  const handlePageChange = (selectedPage) => {
+    handlePageClick(selectedPage);
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
   return (
     <ReactPaginate
       previousLabel={'Prev'}
@@ -10,7 +18,7 @@ export default function Pagination({ pageCount, handlePageClick, currentPage }) 
       pageCount={pageCount}
       marginPagesDisplayed={1}
       pageRangeDisplayed={2}
-      onPageChange={handlePageClick}
+      onPageChange={handlePageChange}
       containerClassName={'pagination'}
       activeClassName={'active'}
       forcePage={currentPage}
